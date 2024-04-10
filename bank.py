9711972f74862cb0f414774f5ee2734e7df01893
Mwikali <mwikali119@gmail.com>
2024-04-10 09:28:17 +0300

    Implement function that runs the server on port 8000 till the server is stopped

--- bank.py
+++ bank.py
@@ -95,4 +95,11 @@ class BankAccount(BaseHTTPRequestHandler):
             self.set_headers(400)
             self.wfile.write(json.dumps({'error': str(e)}).encode())
 
+def run(server_class=HTTPServer, handler_class=BankAccount, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Starting httpd on port {port}...')
+    httpd.serve_forever()
 
+if __name__ == "__main__":
+    run()
